e1eea31536adb904ade2da8970300f22163f59e7
Michael Prather <mprather1@gmail.com>
2017-04-30 16:20:34 +0000

    standard fix

--- src/pagurbation.js
+++ src/pagurbation.js
@@ -1,24 +1,24 @@
 export default function paginate (req, data, pageSize) {
-  let total, 
-    pageCount, 
-    currentPage = 1, 
-    modelsArrays = [], 
-    modelsList = [],
-    object = {},
-    pageData = {}
-    
+  let total
+  let pageCount
+  let currentPage = 1
+  let modelsArrays = []
+  let modelsList = []
+  let object = {}
+  let pageData = {}
+
   total = data.length
-  
+
   while (data.length > 0) {
     modelsArrays.push(data.splice(0, pageSize))
   }
 
   if (typeof req.query.page !== 'undefined') {
-    currentPage = + req.query.page
+    currentPage = +req.query.page
   }
-  
+
   modelsList = modelsArrays[+currentPage - 1]
-  
+
   pageCount = total / pageSize
 
   if (parseInt(pageCount) - pageCount > 0) {
@@ -26,18 +26,18 @@ export default function paginate (req, data, pageSize) {
   } else {
     pageCount = parseInt(pageCount + 1)
   }
-  
+
   pageData = {
     pageSize: pageSize,
     total: total,
     pageCount: pageCount,
-    currentPage: currentPage  
+    currentPage: currentPage
   }
-  
+
   object = {
     models: modelsList,
     pageData: pageData
   }
-  
+
   return object
 }
