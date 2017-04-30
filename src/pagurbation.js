4a5bdc5aec2788b10095c8744a46a2c210cd9ab5
Michael Prather <mprather1@gmail.com>
2017-04-30 16:18:39 +0000

    pagurbation.js

--- src/pagurbation.js
+++ src/pagurbation.js
@@ -1,1 +1,43 @@
-// pagurbation
+export default function paginate (req, data, pageSize) {
+  let total, 
+    pageCount, 
+    currentPage = 1, 
+    modelsArrays = [], 
+    modelsList = [],
+    object = {},
+    pageData = {}
+    
+  total = data.length
+  
+  while (data.length > 0) {
+    modelsArrays.push(data.splice(0, pageSize))
+  }
+
+  if (typeof req.query.page !== 'undefined') {
+    currentPage = + req.query.page
+  }
+  
+  modelsList = modelsArrays[+currentPage - 1]
+  
+  pageCount = total / pageSize
+
+  if (parseInt(pageCount) - pageCount > 0) {
+    pageCount = parseInt(pageCount)
+  } else {
+    pageCount = parseInt(pageCount + 1)
+  }
+  
+  pageData = {
+    pageSize: pageSize,
+    total: total,
+    pageCount: pageCount,
+    currentPage: currentPage  
+  }
+  
+  object = {
+    models: modelsList,
+    pageData: pageData
+  }
+  
+  return object
+}
